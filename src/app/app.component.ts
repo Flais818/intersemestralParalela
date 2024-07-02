5521fce648cdf50d419eac7f149f56ac3c5b8b7c
Fabio Trujillo <fabiotrujillogaribay81@gmail.com>
2024-07-02 17:55:39 -0600

    Busqueda con mas de un termino

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -45,24 +45,133 @@ export class AppComponent implements OnInit {
   }
 
   condicionABuscar(value: string) {
-    let color = value.toString();
-    let inputCondicion = document.getElementById("condicion") as HTMLInputElement;
+    let inputColor = document.getElementById("color") as HTMLInputElement;
+    let inputMarca = document.getElementById("marca") as HTMLInputElement;
+    let inputAnio = document.getElementById("anio") as HTMLInputElement;
 
-    console.time('for-loop');
-
-    for (this.j = 0; this.j < this.autos.length; this.j++) {
-      if (this.autos[this.j].color === inputCondicion.value) {
-        this.contador++;
-        this.ids.push(this.autos[this.j].id);
+    //Por color
+    if (!inputAnio.value && !inputMarca.value) {
+      console.time('for-loop');
+      for (this.j = 0; this.j < this.autos.length; this.j++) {
+        if (this.autos[this.j].color === inputColor.value) {
+          this.contador++;
+          this.ids.push(this.autos[this.j].id);
+        }
       }
+      console.timeEnd('for-loop');
 
+      console.time('filter');
+      const autosVerdes = this.autos.filter(auto => auto.color === inputColor.value);
+      this.cantidadAutosVerdes = autosVerdes.length;
+      console.timeEnd('filter');
     }
-    console.timeEnd('for-loop');
 
-    console.time('filter');
-    const autosVerdes = this.autos.filter(auto => auto.color === inputCondicion.value);
-    this.cantidadAutosVerdes = autosVerdes.length;
-    console.timeEnd('filter');
+    // Por marca
+    else if (!inputAnio.value && !inputColor.value) {
+      console.time('for-loop');
+      for (this.j = 0; this.j < this.autos.length; this.j++) {
+        if (this.autos[this.j].marca === inputMarca.value) {
+          this.contador++;
+          this.ids.push(this.autos[this.j].id);
+        }
+      }
+      console.timeEnd('for-loop');
+
+      console.time('filter');
+      const autosVerdes = this.autos.filter(auto => auto.marca === inputMarca.value);
+      this.cantidadAutosVerdes = autosVerdes.length;
+      console.timeEnd('filter');
+    }
+
+    // Por anio
+    else if (!inputMarca.value && !inputColor.value) {
+      console.time('for-loop');
+      for (this.j = 0; this.j < this.autos.length; this.j++) {
+        if (this.autos[this.j].anio === inputAnio.value) {
+          this.contador++;
+          this.ids.push(this.autos[this.j].id);
+        }
+      }
+      console.timeEnd('for-loop');
+
+      console.time('filter');
+      const autosVerdes = this.autos.filter(auto => auto.anio === inputAnio.value);
+      this.cantidadAutosVerdes = autosVerdes.length;
+      console.timeEnd('filter');
+    }
+
+    // Por color y anio
+    else if (!inputMarca.value) {
+      console.time('for-loop');
+      for (this.j = 0; this.j < this.autos.length; this.j++) {
+        if (this.autos[this.j].anio === inputAnio.value && this.autos[this.j].color === inputColor.value) {
+          this.contador++;
+          this.ids.push(this.autos[this.j].id);
+        }
+      }
+      console.timeEnd('for-loop');
+
+      console.time('filter');
+      const autosVerdes = this.autos.filter(auto => auto.anio === inputAnio.value && auto.color === inputColor.value);
+      this.cantidadAutosVerdes = autosVerdes.length;
+      console.timeEnd('filter');
+    }
+
+    // Por anio y marca
+    else if (!inputColor.value) {
+      console.time('for-loop');
+      for (this.j = 0; this.j < this.autos.length; this.j++) {
+        if (this.autos[this.j].anio === inputAnio.value && this.autos[this.j].marca === inputMarca.value) {
+          this.contador++;
+          this.ids.push(this.autos[this.j].id);
+        }
+      }
+      console.timeEnd('for-loop');
+
+      console.time('filter');
+      const autosVerdes = this.autos.filter(auto => auto.anio === inputAnio.value && auto.marca === inputMarca.value);
+      this.cantidadAutosVerdes = autosVerdes.length;
+      console.timeEnd('filter');
+    }
+
+    // Por marca y color
+    else if (!inputAnio.value) {
+      console.time('for-loop');
+      for (this.j = 0; this.j < this.autos.length; this.j++) {
+        if (this.autos[this.j].color === inputColor.value && this.autos[this.j].marca === inputMarca.value) {
+          this.contador++;
+          this.ids.push(this.autos[this.j].id);
+        }
+      }
+      console.timeEnd('for-loop');
+
+      console.time('filter');
+      const autosVerdes = this.autos.filter(auto => auto.color === inputColor.value && auto.marca === inputMarca.value);
+      this.cantidadAutosVerdes = autosVerdes.length;
+      console.timeEnd('filter');
+    }
+
+    // Por los 3
+    else if (inputAnio.value && inputColor.value && inputMarca.value) {
+      console.time('for-loop');
+      for (this.j = 0; this.j < this.autos.length; this.j++) {
+        if (this.autos[this.j].color === inputColor.value &&
+          this.autos[this.j].marca === inputMarca.value &&
+          this.autos[this.j].anio === inputAnio.value) {
+          this.contador++;
+          this.ids.push(this.autos[this.j].id);
+        }
+      }
+      console.timeEnd('for-loop');
+
+      console.time('filter');
+      const autosVerdes = this.autos.filter(auto => auto.color === inputColor.value
+        && auto.marca === inputMarca.value
+        && auto.anio === inputAnio.value);
+      this.cantidadAutosVerdes = autosVerdes.length;
+      console.timeEnd('filter');
+    }
+
 
     console.log(this.ids)
 
